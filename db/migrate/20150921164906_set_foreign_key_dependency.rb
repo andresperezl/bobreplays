0843cb20929bde480550be0a6a92aec443248199
Andres Perez <perezleon@mail.usf.edu>
2015-09-21 13:05:16 -0400

    fix migration

--- db/migrate/20150921164906_set_foreign_key_dependency.rb
+++ db/migrate/20150921164906_set_foreign_key_dependency.rb
@@ -4,6 +4,10 @@ class SetForeignKeyDependency < ActiveRecord::Migration
     remove_column :votes, :video_id
     remove_column :video_playings, :video_id
 
+    add_column :vote_counts, :video_id, :integer
+    add_column :votes, :video_id, :integer
+    ADD_column :video_playings, :video_id, :integer
+
     add_foreign_key :vote_counts, :videos, on_delete: :cascade
     add_foreign_key :votes, :videos, on_delete: :cascade
     add_foreign_key :video_playings, :videos, on_delete: :cascade
