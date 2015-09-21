b48d298c22a1bc7ecdb7a0f2f9f3fead9ade9eb0
Andres Perez <perezleon@mail.usf.edu>
2015-09-21 12:57:47 -0400

    added dependency for fkeys

---
+++ db/migrate/20150921164906_set_foreign_key_dependency.rb
@@ -0,0 +1,11 @@
+class SetForeignKeyDependency < ActiveRecord::Migration
+  def change
+    remove_column :vote_counts, :video_id
+    remove_column :votes, :video_id
+    remove_column :video_playings, :video_id
+
+    add_foreign_key :vote_counts, :videos, on_delete: :cascade
+    add_foreign_key :votes, :videos, on_delete: :cascade
+    add_foreign_key :video_playings, :videos, on_delete: :cascade
+  end
+end
